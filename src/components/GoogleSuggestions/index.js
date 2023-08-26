a67e4faae163cc80deceb303f356d409f4fd31f3
Rohith Patel <rohithgithub999@gmail.com>
2023-08-26 18:25:53 +0000

    without using lowerCase method

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -34,7 +34,7 @@ class GoogleSuggestions extends Component {
     const {searchInput} = this.state
 
     const searchResultsArray = suggestionsList.filter(eachObjectFilter =>
-      eachObjectFilter.suggestion.toLowerCase().includes(searchInput),
+      eachObjectFilter.suggestion.includes(searchInput),
     )
 
     return (
